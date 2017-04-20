8e367ab256564df61ce337629ce2bccea125ea88
Michiel Borkent <michielborkent@gmail.com>
2017-04-20 11:49:04 +0200

    Upgrade deps and include Yada

---
+++ code/animals-crud/project.clj
@@ -0,0 +1,47 @@
+(defproject animals-crud "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.521"]
+                 [org.clojure/core.async "0.3.442"]
+                 [ring-server "0.4.0"]
+                 [compojure "1.5.2"]
+                 [org.webjars/bootstrap "3.3.6"]
+                 [cljs-http "0.1.42"]
+                 [liberator "0.14.1"]
+                 [fogus/ring-edn "0.3.0"]
+                 [clj-json "0.5.3"]
+                 [hiccup "1.0.5"]
+                 [reagent "0.6.1"]
+                 [prismatic/schema "1.1.1"]
+                 [org.clojure/java.jdbc "0.5.8"]
+                 [com.h2database/h2 "1.4.194"]
+
+                 ;; Yada
+                 [yada "1.2.0"]
+                 [aleph "0.4.1"]
+                 [bidi "2.0.16"]]
+
+  :plugins [[lein-cljsbuild "1.1.3"]]
+
+  :clean-targets ^{:protect false} [:target-path :compile-path "resources/public/out"]
+
+  :source-paths ["src"]
+
+  :profiles {:dev {:plugins [[lein-figwheel "0.5.4-7"]]
+                   :figwheel {:http-server-root "public"
+                              :server-port 3449}}}
+
+  :cljsbuild {:builds [{:id "reagent"
+                        :source-paths ["src-cljs/"]
+                        :figwheel true
+                        :compiler {:output-to "resources/public/crud.js"
+                                   :output-dir "resources/public/out"
+                                   :optimizations :none
+                                   :asset-path "out"
+                                   :main "animals.crud"
+                                   :source-map true}}]}
+
+  :repl-options {:init-ns animals.repl}
+  
+  :global-vars {*print-length* 20})
---
+++ code/animals-crud/src/animals/repl.clj
@@ -0,0 +1,48 @@
+(ns animals.repl
+  (:require [ring.server.standalone :refer [serve]]
+            [animals.api :refer [handler init]]
+            [yada.yada :as yada]
+            [bidi.vhosts :refer [vhosts-model]]))
+
+(defonce server (atom nil))
+
+(defn start-server
+  "used for starting the server in development mode from REPL"
+  [& [port]]
+  (let [port (if port (Integer/parseInt port) 8080)]
+    (reset! server
+            (serve #'handler
+                   {:port port
+                    :init init
+                    :auto-reload? true                    
+                    :join true
+                    :open-browser? false}))
+    (println (str "You can view the site at http://localhost:" port))))
+
+(defn stop-server []
+  (.stop @server)
+  (reset! server nil))
+
+(defonce yada-listener (atom nil))
+
+(defn yada-routes []
+  [#".*" handler])
+
+(defn start-yada []
+  (let [vhosts-model (vhosts-model [:* (yada-routes)])
+        listener (yada/listener vhosts-model
+                                {:port 8090})]
+    (reset! yada-listener listener)
+    (println "Started yada")))
+
+(defn stop-yada []
+  (let [listener @yada-listener]
+    (if-let [close (:close listener)]
+      (close))
+    (reset! yada-listener nil)))
+
+;;;; Scratch
+(comment
+  (start-yada)
+  (stop-yada)
+  )
